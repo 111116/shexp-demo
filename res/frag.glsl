d5f22966318fd487f47c1d24a54b4d198804df13
bilibili <fenggaobuke@gmail.com>
2021-04-28 16:21:17 +0800

    shader frameworking

--- res/frag.glsl
+++ res/frag.glsl
@@ -3,13 +3,18 @@
 
 #define PI (3.14159265359)
 
+#define sh_order 3
 #define N 9
 
 
-in vec3 v_normal;
+in vec3 position; // world position (attribute interpolated)
+in vec3 normal; // world normal (attribute interpolated)
 out vec4 o_color;
-uniform vec3 u_coefficients[9];
-uniform samplerCubeArray u_LHcubemap;
+uniform vec3 u_coefficients[9]; // unused
+uniform samplerCubeArray u_LHcubemap; // (dir, SHindex(l,m)) => L*H(dir), where L is environment light and H is cosine-weighted hemisphere
+uniform samplerCubeArray u_sh_lut; // (dir, SHindex(l,m)) => evaluation of SH basis (l,m) at dir
+uniform sampler2D u_log_lut; // (l, half angle) => V of sphere blocker, SH-projected, value at band l
+uniform sampler2D u_ab_lut; // TODO, currently: (x, half angle) => coefficient, needs to be converted to be function of magnitude
 uniform vec4 u_sphere[30]; // vec4(position, radius)
 
 // TODO: windowing
@@ -17,7 +22,7 @@ uniform vec4 u_sphere[30]; // vec4(position, radius)
 float[N] shexp(float[N] f)
 {
     // naive linear
-    float[9] g;
+    float[N] g;
     float e = exp(f[0]/sqrt(4.0/PI));
     g[0] = sqrt(4.0*PI)*e;
     for (int i = 1; i < N; i++)
@@ -27,19 +32,63 @@ float[N] shexp(float[N] f)
     return g;
 }
 
+float[N] rotate(float[sh_order] a, vec3 w)
+{
+    float y[N];
+    for (int j=0; j<N; ++j)
+        y[j] = texture(u_sh_lut, vec4(w, j)).x;
+    for (int l=0; l<sh_order; ++l) {
+        float k = a[l] * sqrt(4*PI/(2*l+1));
+        for (int m=-l; m<=l; ++m)
+            y[l*l+l+m] *= k;
+    }
+    return y;
+}
+
+float shdot(float[N] a, float[N] b)
+{
+    float s = 0;
+    for (int j=0; j<N; ++j)
+        s += a[j] * b[j];
+    return s;
+}
+
+vec3 shdot(float[N] a, vec3[N] b)
+{
+    vec3 s = vec3(0,0,0);
+    for (int j=0; j<N; ++j)
+        s += a[j] * b[j];
+    return s;
+}
+
 void main()
 {
 // for all sphere blockers
 // calculate angle & rotation
 // accumulate log(occu)
-    vec3 n = normalize(v_normal);
-    vec3 SHLightResult[9];
+    vec3 n = normalize(normal);
+    // vec3 SHLightResult[N];
+
+    float[N] f = float[N](0,0,0,0,0,0,0,0,0);
+
+    // for (int i=0; i<30; ++i) {
+    //     vec3 v = u_sphere[i].xyz - position;
+    //     float dist = length(v);
+    //     float angle = asin(u_sphere[i].w / dist);
+    //     float[sh_order] cur_symmlog;
+    //     for (int l=0; l<sh_order; ++l)
+    //         cur_symmlog[l] = texture(u_log_lut, vec2((l+0.5)/sh_order, angle/(PI/2))).x;
+    //     float[N] cur_log = rotate(cur_symmlog, normalize(v));
+    //     for (int j=0; j<N; ++j)
+    //         f[j] += cur_log[j];
+    // }
+    float[N] g = shexp(f);
 
     // for (int i=0; i<9; ++i)
        //  SHLightResult[i] = texture(u_LHcubemap, vec4(n,i)).rgb;
   //   logv;
   //   for (int i=0; i<nspheres; ++i) {
-  //    vec3 distv = world_position - u_position[i];
+  //    vec3 distv = position - u_position[i];
   //    float dist = length(distv);
         // float angle = asin(u_radius[i] / dist);
         // t = texture()
@@ -50,7 +99,11 @@ void main()
     // for (int i = 0; i < 9; ++i)
     //     result += SHLightResult[i];
 
-    vec3 result = 3.5449075 * texture(u_LHcubemap, vec4(n,0)).rgb;
+    // vec3 result = 3.5449075 * texture(u_LHcubemap, vec4(n,0)).rgb;
+    vec3 LH[N];
+    for (int i = 0; i < 9; ++i)
+        LH[i] = texture(u_LHcubemap, vec4(n,i)).rgb;
+    vec3 result = shdot(g, LH);
     // L_H dot product with SH_one, yields the integral of L_H
 
     result = 0.8 / 3.1416 * result; // times brdf
--- res/vert.glsl
+++ res/vert.glsl
@@ -3,10 +3,12 @@ in vec3 a_position;
 in vec3 a_normal;
 uniform mat4 u_view;
 uniform mat4 u_projection;
-out vec3 v_normal;
+out vec3 normal;
+out vec3 position;
 
 void main()
 {
     gl_Position = u_projection * (u_view * vec4(a_position, 1.0));
-    v_normal = a_normal;
+    position = a_position;
+    normal = a_normal;
 }
