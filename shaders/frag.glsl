b2367b63b659788cbd78b85f3a9525f70b78debe
bilibili <fenggaobuke@gmail.com>
2021-05-16 18:18:41 +0800

    fix texture gamma correction

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -10,12 +10,12 @@ uniform sampler2DArray albedo_map;
 
 void main()
 {
-    // gamma correction
-    float gamma = 2.2;
     vec3 t = max(color, 0.0);
-    t = pow(t, vec3(1.0/gamma));
     // objects without texture are marked with texcoord (-1,-1)
     if (texcoord.x > -0.5 && texcoord.y > -0.5)
     	t *= texture(albedo_map, vec3(texcoord.x, 1-texcoord.y, objid)).xyz;
+    // gamma correction
+    float gamma = 2.2;
+    // t = pow(t, vec3(1.0/gamma));
     o_color = vec4(t, 1.0);
 }
