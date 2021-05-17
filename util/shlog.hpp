b62793b9ebb6d6254ad3aa765a0c859ce4020b23
bilibili <fenggaobuke@gmail.com>
2021-05-17 22:17:41 +0800

    fix makelut & improve eigen precision; close #7

--- util/shlog.hpp
+++ util/shlog.hpp
@@ -1,11 +1,12 @@
 #pragma once
 
+#include <algorithm>
 #include "sh.hpp"
 #include "lib/Eigen/Eigen"
 
 
 template <int n>
-SH<n> eigen_to_sh(Eigen::Matrix<float, n*n, 1> v)
+SH<n> eigen_to_sh(Eigen::Matrix<double, n*n, 1> v)
 {
 	SH<n> a;
 	for (int i=0; i<n*n; ++i)
@@ -14,9 +15,9 @@ SH<n> eigen_to_sh(Eigen::Matrix<float, n*n, 1> v)
 }
 
 template <int n>
-Eigen::Matrix<float, n*n, 1> sh_to_eigen(SH<n> a)
+Eigen::Matrix<double, n*n, 1> sh_to_eigen(SH<n> a)
 {
-	Eigen::Matrix<float, n*n, 1> v;
+	Eigen::Matrix<double, n*n, 1> v;
 	for (int i=0; i<n*n; ++i)
 		v(i) = a.a[i];
 	return v;
@@ -26,14 +27,14 @@ Eigen::Matrix<float, n*n, 1> sh_to_eigen(SH<n> a)
 template <int n>
 SH<n> log(const SH<n>& a)
 {
-    typedef Eigen::Matrix<float, n*n, n*n> EMatrix;
-    EMatrix Mf;
+    using Eigen::MatrixXd;
+    MatrixXd Mf = MatrixXd::Zero(n*n, n*n);
     matrix<n*n> Mf_tmp = a.prodMatrix();
     for (int i=0; i<n*n; ++i)
     for (int j=0; j<n*n; ++j)
         Mf(i,j) = Mf_tmp.a[i][j];
     // Eigendecomposition Mf = R^T D R
-    Eigen::SelfAdjointEigenSolver<EMatrix> eigensolver(Mf);
+    Eigen::SelfAdjointEigenSolver<MatrixXd> eigensolver(Mf);
     if (eigensolver.info() != Eigen::Success)
     	throw "Eigendecomposition failed";
     auto RT = eigensolver.eigenvectors();
@@ -41,16 +42,15 @@ SH<n> log(const SH<n>& a)
     assert(-D.minCoeff() <= D.maxCoeff());
     double epsilon = 0.02 * D.maxCoeff();
     // calculate q'(max(D,eps))
-    EMatrix qD;
-    qD.setZero();
+    MatrixXd qD = MatrixXd::Zero(n*n, n*n);
     auto diag = qD.diagonal();
     for (int i=0; i<n*n; ++i)
     {
-    	float t = fmax(epsilon, D(i));
+    	double t = std::max(epsilon, D(i));
     	diag[i] = log(t) / (t-1);
     }
     // log(f) = R^T q'(D) R (f-1)
-    Eigen::Matrix<float, n*n, 1> f_minus_unit = sh_to_eigen(a) - sh_to_eigen(SH<n>::unit());
+    Eigen::Matrix<double, n*n, 1> f_minus_unit = sh_to_eigen(a) - sh_to_eigen(SH<n>::unit());
     auto logf = RT * qD * RT.transpose() * f_minus_unit;
     return eigen_to_sh<n>(logf);
 }
